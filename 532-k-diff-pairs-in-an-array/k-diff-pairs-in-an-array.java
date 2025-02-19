a2ffc68ea3ff19120f03bd61828a8940009cab4e
Mohammad Daud <119299628+mohammaddaud88@users.noreply.github.com>
2025-02-19 11:30:21 +0530

    Time: 6 ms (95.60%) | Memory: 44.9 MB (56.17%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
@@ -4,15 +4,32 @@ class Solution {
         int j=0;
         int n = nums.length;
         Arrays.sort(nums);
-        for(int i=0;i<n;i++){
-            while(j<n && nums[j]-nums[i] <k){
-                j++;
-            }
-            if(j<n && nums[j]-nums[i] == k && i != j){
+        // for(int i=0;i<n;i++){
+        //     while(j<n && nums[j]-nums[i] <k){
+        //         j++;
+        //     }
+        //     if(j<n && nums[j]-nums[i] == k && i != j){
+        //         count++;
+        //         while(j+1<n && nums[j] == nums[j+1]){
+        //             j++;
+        //         }
+        //         j++;
+        //     }
+        // }
+
+        int i=0;
+        j=0;
+        
+        while(i<n && j<n){
+            if(j<n && nums[j]-nums[i] == k && i!= j){
                 count++;
-                while(j+1<n && nums[j] == nums[j+1]){
+                while(j+1<n && nums[j]== nums[j+1]){
                     j++;
                 }
+                j++;
+            } else if(nums[j]-nums[i] > k){
+                i++;
+            } else {
                 j++;
             }
         }
