de5f5b41ffc5bd3f188165d0c47da1d47ace90f6
Mohammad Daud <119299628+mohammaddaud88@users.noreply.github.com>
2025-02-25 13:21:49 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (61.38%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -14,23 +14,43 @@
  * }
  */
 class Solution {
-    // return height and diameter of the subtree
-    int [] solve(TreeNode root){
+    // method - 2
+    int ans=0;
+    int solve(TreeNode root){
         if(root == null){
-            return new int[]{0,0} ;
+            return 0;
         }
         if(root.left == null && root.right == null){
-            return new int[]{1,0};
+            return 1;
         }
+        int leftTree = solve(root.left);
+        int rightTree = solve(root.right);
+        // ans = Math.max(Math.max(leftTree,rightTree),leftTree+rightTree);
+        ans= Math.max((leftTree + rightTree),ans);
+        return Math.max(leftTree,rightTree)+1;
+    }
 
-        int [] left = solve(root.left);
-        int [] right = solve(root.right);
-        int ans[]=new int[2];
-        ans[0]=Math.max(left[0],right[0])+1;
-        ans[1]= Math.max(Math.max(left[1],right[1]),left[0]+right[0]);
+    // method - 1
+    // return height and diameter of the subtree
+    // int [] solve(TreeNode root){
+    //     if(root == null){
+    //         return new int[]{0,0} ;
+    //     }
+    //     if(root.left == null && root.right == null){
+    //         return new int[]{1,0};
+    //     }
+
+    //     int [] left = solve(root.left);
+    //     int [] right = solve(root.right);
+    //     int ans[]=new int[2];
+    //     ans[0]=Math.max(left[0],right[0])+1;
+    //     ans[1]= Math.max(Math.max(left[1],right[1]),left[0]+right[0]);
+    //     return ans;
+    // }
+    public int diameterOfBinaryTree(TreeNode root) {
+        // return solve(root)[1];
+        solve(root);
         return ans;
     }
-    public int diameterOfBinaryTree(TreeNode root) {
-        return solve(root)[1];
-    }
+
 }
