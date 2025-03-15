a9c755165251777f31a45dfec365bf0150f6e891
Mohammad Daud <119299628+mohammaddaud88@users.noreply.github.com>
2025-03-15 23:08:18 +0530

    Time: 9 ms (98.12%) | Memory: 47.1 MB (70.13%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,12 +1,12 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int low =0;
-        int high = (int) 1e8;
-        int ans=-1;
-        while(low<=high){
-            int mid = low+(high -low)/2;
+        int low = 0;
+        int high = (int ) 1e8;
+        int ans =-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
             if(isPossible(weights,mid,days)){
-                ans=mid;
+                ans = mid;
                 high = mid-1;
             } else {
                 low = mid+1;
@@ -14,21 +14,19 @@ class Solution {
         }
         return ans;
     }
-    public boolean isPossible(int [] arr, int target , int days){
-        int day=1;
-        int sum=0;
-        int n = arr.length;
-        for(int i=0;i<n;i++){
-            if(arr[i] > target){
+    public boolean isPossible(int [] arr, int target, int days){
+        int day =1;
+        int sum =0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>target){
                 return false;
-            } else if(arr[i]+sum <= target){
-                sum += arr[i];
+            } else if(arr[i]+sum <=target){
+                sum +=arr[i];
             } else {
+                sum=arr[i];
                 day++;
-                sum = arr[i];
             }
-             
         }
-        return day <= days;
+        return day<=days;
     }
 }
