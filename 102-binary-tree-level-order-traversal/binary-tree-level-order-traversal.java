06fe5b3bc0733e3c7c80d3491b520218ccebe003
Mohammad Daud <119299628+mohammaddaud88@users.noreply.github.com>
2025-03-15 12:45:52 +0530

    Time: 1 ms (89.58%) | Memory: 45.2 MB (21.95%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,39 +1,43 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
         List<List<Integer>> lists = new ArrayList<>();
-        if(root != null){
-            q.offer(root);
+        if (root == null) {
+            return lists;
         }
-        while(q.size()>0){
-            int numElementOnSameLevel = q.size();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int currLvl = 0;
+        while (!q.isEmpty()) {
             List<Integer> list = new ArrayList<>();
-            for(int i=0;i<numElementOnSameLevel;i++){
-                TreeNode curr= q.poll();
-                list.add(curr.val);
-                if(curr.left != null){
-                    q.offer(curr.left);
-                } 
-                if(curr.right != null){
-                    q.offer(curr.right);
+            lists.add(list);
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                lists.get(currLvl).add(curr.val);
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
                 }
             }
-            lists.add(list);
+
+            currLvl++;
         }
         return lists;
     }
