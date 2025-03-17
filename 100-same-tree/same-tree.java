5b067ec72d382bb673bf9e2d22008607439be797
Mohammad Daud <119299628+mohammaddaud88@users.noreply.github.com>
2025-03-17 23:16:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (52.52%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -19,36 +19,41 @@ class Solution {
             return true;
         if (p == null || q == null)
             return false;
-        Queue<TreeNode> q1 = new LinkedList<>();
-        Queue<TreeNode> q2 = new LinkedList<>();
-        q1.add(p);
-        q2.add(q);
-        while (!q1.isEmpty() && !q2.isEmpty()) {
-            int size = q1.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode curr1 = q1.poll();
-                TreeNode curr2 = q2.poll();
-                if (curr1.val != curr2.val) {
-                    return false;
-                }
-                if (curr1.left != null && curr2.left != null) {
-                    q1.add(curr1.left);
-                    q2.add(curr2.left);
-                } else if(curr1.left == null && curr2.left != null){
-                    return false;
-                } else if(curr1.left != null && curr2.left == null){
-                    return false;
-                }
-                if (curr1.right != null && curr2.right != null) {
-                    q1.add(curr1.right);
-                    q2.add(curr2.right);
-                }else if(curr1.right != null && curr2.right == null){
-                    return false;
-                }else if(curr1.right == null && curr2.right != null){
-                    return false;
-                }
-            }
-        }
-        return true;
+            // solution using DFS
+            return p.val==q.val &&(isSameTree(p.left,q.left)&&isSameTree(p.right,q.right));
+
+
+            // solution using BFS
+        // Queue<TreeNode> q1 = new LinkedList<>();
+        // Queue<TreeNode> q2 = new LinkedList<>();
+        // q1.add(p);
+        // q2.add(q);
+        // while (!q1.isEmpty() && !q2.isEmpty()) {
+        //     int size = q1.size();
+        //     for (int i = 0; i < size; i++) {
+        //         TreeNode curr1 = q1.poll();
+        //         TreeNode curr2 = q2.poll();
+        //         if (curr1.val != curr2.val) {
+        //             return false;
+        //         }
+        //         if (curr1.left != null && curr2.left != null) {
+        //             q1.add(curr1.left);
+        //             q2.add(curr2.left);
+        //         } else if(curr1.left == null && curr2.left != null){
+        //             return false;
+        //         } else if(curr1.left != null && curr2.left == null){
+        //             return false;
+        //         }
+        //         if (curr1.right != null && curr2.right != null) {
+        //             q1.add(curr1.right);
+        //             q2.add(curr2.right);
+        //         }else if(curr1.right != null && curr2.right == null){
+        //             return false;
+        //         }else if(curr1.right == null && curr2.right != null){
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
     }
 }
