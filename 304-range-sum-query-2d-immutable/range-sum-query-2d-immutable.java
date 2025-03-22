25badade80e58b4889fe3102451ae9de9b66935e
Mohammad Daud <119299628+mohammaddaud88@users.noreply.github.com>
2025-03-23 02:59:03 +0530

    Time: 110 ms (27.58%) | Memory: 67 MB (78.30%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,20 +1,21 @@
 class NumMatrix {
-    int [][] prefix;
+    int preSum[][];
+
     public NumMatrix(int[][] matrix) {
-         prefix = new int[matrix.length+1][matrix[0].length+1];
+        preSum = new int[matrix.length+1][matrix[0].length+1];
         for(int i=1;i<=matrix.length;i++){
             for(int j=1;j<=matrix[0].length;j++){
-                prefix[i][j] += prefix[i-1][j]+prefix[i][j-1] - prefix[i-1][j-1] + matrix[i-1][j-1];
+                preSum[i][j] += preSum[i-1][j] + preSum[i][j-1] + matrix[i-1][j-1] - preSum[i-1][j-1];
             }
         }
     }
-
+    
     public int sumRegion(int row1, int col1, int row2, int col2) {
         row1++;
-        col1++;
         row2++;
+        col1++;
         col2++;
-        return prefix[row2][col2] - prefix[row1-1][col2] - prefix[row2][col1-1] + prefix[row1-1][col1-1];
+        return preSum[row2][col2] - preSum[row2][col1-1]-preSum[row1-1][col2] + preSum[row1-1][col1-1];
     }
 }
 
