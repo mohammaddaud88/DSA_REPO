fe5fb5bbe50c59b854bef5e33891913fec5794d0
Mohammad Daud <119299628+mohammaddaud88@users.noreply.github.com>
2025-03-24 00:39:20 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (67.62%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,32 +1,35 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        long low =0;
-        long high = (int) 1e9;
-        long ans =high;
-        while(low<=high){
-            long mid = low + (high -low)/2;
+        // for(int i=1;i<nums.length;i++){
+        //     nums[i] += nums[i-1];
+        // }
+        int i=0;
+        int j = (int) 1e9;
+        int ans =-1;
+        while(i<=j){
+            int mid = i + (j-i)/2;
             if(isPossible(nums,mid,k)){
                 ans = mid;
-                high = mid-1;
+                j = mid-1;
             } else {
-                low = mid+1;
+                i = mid+1;
             }
         }
-        return (int) ans;
+        return ans;
     }
-    public boolean isPossible(int [] nums, long target, long maxPart){
-        long part = 1;
-        long sum =0;
+    public boolean isPossible(int [] nums, int mid, int k){
+        int part = 1;
+        int sum =0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i] > target){
+            if(nums[i] > mid){
                 return false;
-            } else if(nums[i] + sum <= target){
+            } else if(sum + nums[i] <=mid){
                 sum += nums[i];
-            } else {
-                part++;
+            } else{
                 sum = nums[i];
+                part++;
             }
         }
-        return part <= maxPart;
+        return part <= k;
     }
 }
