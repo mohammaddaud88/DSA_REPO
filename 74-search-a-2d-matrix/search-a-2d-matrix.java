c4de9f85b310729004ace575ad2338a45ed0daa0
Mohammad Daud <119299628+mohammaddaud88@users.noreply.github.com>
2025-03-24 19:51:06 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (80.68%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,25 +1,46 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length-1;
-        for(int i=0;i<m;i++){
-            if(binarySearch(matrix[i],target,0,n)){
-                return true;
+            int m = matrix.length;
+            int n = matrix[0].length;
+
+            int low = 0;
+            int high = n*m-1;
+            while(low <= high){
+                int mid = (low + high)/2;
+                if(matrix[mid/n][mid%n] < target){
+                    low = mid+1;
+                } else if(matrix[mid/n][mid%n] > target){
+                    high = mid-1;
+                } else {
+                    return true;
+                }
             }
-        }
-        return false;
+
+            return false;
+
+
+        // bruth force approach
+
+        // int m = matrix.length;
+        // int n = matrix[0].length-1;
+        // for(int i=0;i<m;i++){
+        //     if(binarySearch(matrix[i],target,0,n)){
+        //         return true;
+        //     }
+        // }
+        // return false;
     }
-    public boolean binarySearch(int[] matrix, int target, int low, int high){
-        while(low <= high){
-            int mid = (low+high)/2;
-            if(matrix[mid] == target){
-                return true;
-            } else if(matrix[mid] > target){
-                high = mid-1;
-            } else {
-                low = mid+1;
-            }
-        }
-        return false;
-    }
+    // public boolean binarySearch(int[] matrix, int target, int low, int high){
+    //     while(low <= high){
+    //         int mid = (low+high)/2;
+    //         if(matrix[mid] == target){
+    //             return true;
+    //         } else if(matrix[mid] > target){
+    //             high = mid-1;
+    //         } else {
+    //             low = mid+1;
+    //         }
+    //     }
+    //     return false;
+    // }
 }
